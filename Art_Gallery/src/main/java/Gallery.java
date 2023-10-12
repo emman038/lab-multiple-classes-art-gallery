b3c373e34350bde2996ac51a0b9fe29160adb5f4
Emmanuel Ajayi <emmanuelajayi@Emmanuels-MacBook-Pro.local>
2023-10-12 20:21:00 +0100

    Created Customer Class and associted properties (with setters and getters) using TDD

--- Art_Gallery/src/main/java/Gallery.java
+++ Art_Gallery/src/main/java/Gallery.java
@@ -1,2 +1,38 @@
-// public class Gallery {
-// }
+import java.util.ArrayList;
+
+public class Gallery {
+  private String name;
+  private int till;
+  private ArrayList<Artwork> stock;
+
+  public Gallery(String name, int till, ArrayList<Artwork> stock){
+   this.name = name;
+   this.till = till;
+   this.stock = stock;
+  }
+
+  public String getName() {
+   return this.name;
+  }
+
+  public void setName(String name) {
+   this.name = name;
+  }
+
+  public int getTill() {
+   return this.till;
+  }
+
+  public void setTill(int till) {
+   this.till = till;
+  }
+
+  public ArrayList<Artwork> getStock() {
+   return this.stock;
+  }
+
+  public void setStock(ArrayList<Artwork> stock) {
+   this.stock = stock;
+  }
+
+}
--- Art_Gallery/src/test/java/GalleryTest.java
+++ Art_Gallery/src/test/java/GalleryTest.java
@@ -1,6 +1,71 @@
-// public class GalleryTest {
-//     Gallery gallery;
-// 
-//     @BeforeEach
-// 
-// }
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class GalleryTest {
+     Gallery gallery;
+     Artwork dame;
+     Artwork curry;
+     Artwork lebron;
+     Artist artist;
+     ArrayList<Artwork> stock;
+
+     @BeforeEach
+     public void setUp(){
+         artist = new Artist("Michelangelo");
+
+         dame = new Artwork("dame", artist, 100);
+         curry = new Artwork("curry", artist, 1000);
+         lebron = new Artwork("lebron", artist, 5000);
+
+         stock = new ArrayList<>(Arrays.asList(lebron, curry, dame));
+
+         gallery = new Gallery("Louvre", 10000, stock);
+     }
+
+     @Test
+     public void canGetName(){
+        assertThat(gallery.getName()).isEqualTo("Louvre");
+     }
+
+     @Test
+     public void canSetName(){
+         gallery.setName("Anfield");
+         assertThat(gallery.getName()).isEqualTo("Anfield");
+     }
+
+    @Test
+    public void canGetTill(){
+        assertThat(gallery.getTill()).isEqualTo(10000);
+    }
+
+    @Test
+    public void canSetTill(){
+        gallery.setTill(5000);
+        assertThat(gallery.getTill()).isEqualTo(5000);
+    }
+
+    @Test
+    public void canGetStock(){
+        assertThat(gallery.getStock()).isEqualTo(stock);
+    }
+
+    @Test
+    public void canSetStock(){
+        artist = new Artist("Michelangelo");
+
+        Artwork szobo = new Artwork("Szobo", artist, 100);
+        Artwork endo = new Artwork("Endo", artist, 1000);
+        Artwork salah = new Artwork("Salah", artist, 5000);
+
+        ArrayList<Artwork> stock2 = new ArrayList<>(Arrays.asList(szobo, endo, salah));
+
+        gallery.setStock(stock2);
+        assertThat(gallery.getStock()).isEqualTo(stock2);
+    }
+
+ }
