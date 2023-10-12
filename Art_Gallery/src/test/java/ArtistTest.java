a543a046f376d340a1409eda66a99d18da797f73
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-10-12 16:49:36 +0100

    Created Artist Class with name property. Created setters and getters for name property using TDD. Utilised @BeforeEach annotation for object artist.

---
+++ Art_Gallery/src/test/java/ArtistTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ArtistTest {
+    Artist artist;
+
+    @BeforeEach
+    public void setUp() {
+        artist = new Artist("Picasso");
+    }
+
+    @Test
+    public void canGetName() {
+        assertThat(artist.getName()).isEqualTo("Picasso");
+    }
+
+    @Test
+    public void canSetName(){
+        artist.setName("Michelangelo");
+        String actual = artist.getName();
+        String expected = "Michelangelo";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    
+}
